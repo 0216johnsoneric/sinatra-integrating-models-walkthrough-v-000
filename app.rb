8a3e5a198d6b39be5e07315232b5c8c432f9c640
Eric Johnson <ericjohnson0216@icloud.com>
2020-05-08 00:56:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,8 +15,15 @@ class App < Sinatra::Base
   #   erb :results
   # end
   post '/' do
+<<<<<<< HEAD
     @analyzed_text = TextAnalyzer.new(params[:user_text])
     # x = @analyzed_text
+=======
+    text_from_user = params[:user_text]
+
+    @analyzed_text = TextAnalyzer.new(text_from_user)
+
+>>>>>>> f4c12d674a94f758a5ab1374a1ddff83bd33d30c
     erb :results
   end
 
